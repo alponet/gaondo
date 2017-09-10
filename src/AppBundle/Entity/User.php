0da673f082004e599ec987a89186406610c970fd
Tom <mail@alpers.it>
2017-09-10 21:09:00 +0200

    entitiy User

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Author: tom
+ * Date: 10.09.17
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="`user`")
+ */
+class User
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=128)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $regDate;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $activeDate;
+
+    /**
+     * @ORM\Column(type="string", length=128)
+     */
+    private $avatarUrl;
+
+}
