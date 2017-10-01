f6e2116bc4764598d21fa1096f82401a54eef616
Tom <mail@alpers.it>
2017-10-01 16:19:53 +0200

    register form

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,8 +1,15 @@
 var m = require("mithril");
 var Header = require("./header");
+var Feed = require("./feed");
 var Register = require("./register");
 var Footer = require("./footer");
 
 m.mount(document.getElementById("header"), Header);
-m.mount(document.getElementById("content"), Register);
+
+var content = document.getElementById("content");
+m.route(content, "/feed", {
+    "/feed": Feed,
+    "/register": Register
+});
+
 m.mount(document.getElementById("footer"), Footer);
--- frontend/js/register.js
+++ frontend/js/register.js
@@ -1,14 +1,51 @@
 var m = require("mithril");
 
-module.exports = {
-    view: function () {
-        return m(".register", [
+var Register = {
+    name: '',
+    email: '',
+    status: {},
+    submit: function (e) {
+        e.preventDefault();
+        console.log(Register);
+
+        var formData = new FormData();
+        formData.set("name", Register.name);
+        formData.set("email", Register.email);
+
+        m.request({
+            method: "POST",
+            url: "/u/",
+            data: formData
+        }).then(function (response) {
+            console.log(response);
+            Register.status = response;
+        });
+    },
+    view: function (vnode) {
+        return m(".register",
             m("h1", "register"),
-            m("form", [
-                m("input", "name"),
-                m("input", "email"),
-                m("button", "register")
-            ])
-        ])
+            m("form",
+                m("label", "name"),
+                m("input[type=text][placeholder=name]", {
+                    value: Register.name,
+                    onchange: function(e) {
+                        Register.name = e.currentTarget.value;
+                    }
+                }),
+                m("label", "email"),
+                m("input[placeholder=email]", {
+                    value: Register.email,
+                    onchange: function(e) {
+                        Register.email = e.currentTarget.value;
+                    }
+                }),
+                m(".status", Register.status.hasOwnProperty("errors") ? Register.status.errors.map(function (t) {
+                        return m("label.error", t.detail);
+                    }) : Register.status.hasOwnProperty('success') ? m("label.success", Register.status.success) : ''),
+                m("button", { onclick: Register.submit }, "register")
+            )
+        )
     }
 }
+
+module.exports = Register;
