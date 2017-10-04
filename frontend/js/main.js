563c5ab270378f4cf66d82fe631a6940083d59ae
Tom <mail@alpers.it>
2017-10-04 20:56:44 +0200

    stream frontend routing & view

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,14 +1,14 @@
 var m = require("mithril");
 var Header = require("./header");
-var Feed = require("./feed");
+var Stream = require("./stream");
 var Register = require("./register");
 var Footer = require("./footer");
 
 m.mount(document.getElementById("header"), Header);
 
 var content = document.getElementById("content");
-m.route(content, "feed", {
-    "feed": Feed,
+m.route(content, "stream", {
+    "stream": Stream,
     "register": Register
 });
 
---
+++ frontend/js/stream.js
@@ -0,0 +1,12 @@
+var m = require("mithril");
+
+var Stream = {
+    view: function () {
+        return [
+            m('h2', "Stream"),
+            m('p', "No content.")
+        ];
+    }
+};
+
+module.exports = Stream;
