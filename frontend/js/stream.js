7a65fb54101739e01b70102ef0837e761d3ceb6c
Tom <mail@alpers.it>
2017-10-08 23:02:09 +0200

    Stream view

--- frontend/js/stream.js
+++ frontend/js/stream.js
@@ -1,11 +1,22 @@
 var m = require("mithril");
 
 var Stream = {
+    memes: [],
+    oninit: function () {
+        m.request("/m").then(function (response) {
+            Stream.memes = response;
+        });
+    },
     view: function () {
-        return [
-            m('h2', "Stream"),
-            m('p', "No content.")
-        ];
+        return m(".stream", Stream.memes.map(function (meme) {
+            return m(".meme#m" + meme.id,
+                m("h3", meme.title),
+                m("img[src=" + meme.file + "]"),
+                m(".description", meme.description),
+                m(".date", meme.date.date),
+                m(".author", meme.author)
+            );
+        }));
     }
 };
 
