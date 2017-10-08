b058f05ce1a26e9ba5ae00a3dc8aa8a5ffc44a7b
Tom <mail@alpers.it>
2017-10-08 04:24:10 +0200

    model BasePost

---
+++ src/AppBundle/Entity/BasePost.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Author: tom
+ * Date: 06.10.17
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+
+/**
+ * @ORM\Entity
+ * @ORM\InheritanceType("JOINED")
+ * @ORM\DiscriminatorColumn(name="typeDiscr", type="string")
+ * @ORM\DiscriminatorMap({})
+ */
+class BasePost
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="User", inversedBy="posts")
+     * @ORM\JoinColumn(name="author_id", referencedColumnName="id")
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $creationDate;
+}
