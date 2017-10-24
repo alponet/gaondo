ba8db760626e2ee805e1d246d2b4b8d63fb1cb0c
Tom <mail@alpers.it>
2017-10-24 22:28:57 +0200

    i18n Action

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,18 +4,69 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
+    }
+
+
+    /**
+     * @Route("/i18n/")
+     *
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function i18nAction()
+    {
+        $i18n = $this->get("translator");
+
+        $translations = [
+            "main" => [
+                "login"     => $i18n->trans("main.login"),
+                'logout'    => $i18n->trans('main.logout'),
+                "register"  => $i18n->trans("main.register"),
+                "upload"    => $i18n->trans("main.upload"),
+                "cancel"    => $i18n->trans("main.cancel"),
+                "submit"    => $i18n->trans("main.submit"),
+            ],
+            "meme" => [
+                "description"   => $i18n->trans("meme.description"),
+                "file"          => $i18n->trans("meme.file"),
+                "newMeme"       => $i18n->trans("meme.newMeme"),
+                "title"         => $i18n->trans("meme.title"),
+            ],
+            "profile" => [
+                "changePassword"    => $i18n->trans("profile.changePassword"),
+                "oldPassword"       => $i18n->trans("profile.oldPassword"),
+                "newPassword"       => $i18n->trans("profile.newPassword"),
+                "verifyNewPassword" => $i18n->trans("profile.verifyNewPassword"),
+                "passwordChanged"   => $i18n->trans("profile.passwordChanged"),
+                "deleteAccount"     => $i18n->trans("profile.deleteAccount"),
+                'reallyDeleteAcc'   => $i18n->trans("profile.reallyDeleteAcc"),
+                'notReversible'     => $i18n->trans("profile.notReversible"),
+                'passwordToConfirm' => $i18n->trans("profile.passwordToConfirm"),
+                'accDeleted'        => $i18n->trans("profile.accDeleted"),
+            ],
+            'register' => [
+                'name'     => $i18n->trans('register.name'),
+                'email'    => $i18n->trans('register.email'),
+                'register' => $i18n->trans('register.register'),
+            ],
+            "status" => [
+                "uploading" => $i18n->trans("status.uploading"),
+                'loading'   => $i18n->trans('status.loading'),
+            ]
+        ];
+
+
+        return $this->json($translations);
     }
 }
