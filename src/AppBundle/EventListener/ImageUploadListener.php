aac2ef38b034c1af7d07080deceafd0bc7fb53cf
Tom <mail@alpers.it>
2017-11-17 02:15:43 +0100

    convert image on upload

---
+++ src/AppBundle/EventListener/ImageUploadListener.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Author: tom
+ * Date: 14.11.17
+ */
+
+namespace AppBundle\EventListener;
+
+use Gregwar\Image\Image;
+use Symfony\Component\HttpKernel\HttpKernel;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Vich\UploaderBundle\Event\Event;
+
+class ImageUploadListener
+{
+    /**
+     * @param Event $event
+     */
+    public function convertToJpeg(Event $event)
+    {
+        $mapping = $event->getMapping();
+        $object = $event->getObject();
+
+        $path = Image::open($mapping->getFile($object)->getRealPath())->jpeg();
+        $convertedImage = new UploadedFile($path, '', 'jpeg', 1, null, true);
+
+        $mapping->setFile($object, $convertedImage);
+    }
+}
