c16c8e5dbfd190343680ce89cb311fdfb78b3f9f
Tom <mail@alpers.it>
2018-02-12 23:39:10 +0100

    fix broken migration

--- app/DoctrineMigrations/Version20171202000751.php
+++ app/DoctrineMigrations/Version20171202000751.php
@@ -22,9 +22,9 @@ class Version20171202000751 extends AbstractMigration
 
         $this->addSql('ALTER TABLE user ADD username_canonical VARCHAR(180) NOT NULL');
 	    $this->addSql('ALTER TABLE user ADD email_canonical VARCHAR(180) NOT NULL');
-	    $this->addSql('ALTER TABLE user ADD confirmation_token VARCHAR(180) NOT NULL');
+	    $this->addSql('ALTER TABLE user ADD confirmation_token VARCHAR(180) DEFAULT NULL');
 		$this->addSql('ALTER TABLE user ADD enabled TINYINT(1) NOT NULL');
-		$this->addSql('ALTER TABLE user ADD salt VARCHAR(255) NOT NULL');
+		$this->addSql('ALTER TABLE user ADD salt VARCHAR(255) DEFAULT NULL');
 		$this->addSql('ALTER TABLE user ADD last_login DATETIME DEFAULT NULL');
 		$this->addSql('ALTER TABLE user ADD password_requested_at DATETIME DEFAULT NULL');
 		$this->addSql('ALTER TABLE user ADD roles LONGTEXT NOT NULL COMMENT "(DC2Type:array)"');
@@ -46,6 +46,6 @@ class Version20171202000751 extends AbstractMigration
         $this->addSql('DROP INDEX UNIQ_8D93D64992FC23A8 ON `user`');
         $this->addSql('DROP INDEX UNIQ_8D93D649A0D96FBF ON `user`');
         $this->addSql('DROP INDEX UNIQ_8D93D649C05FB297 ON `user`');
-        $this->addSql('ALTER TABLE user DROP (username_canonical, email_canonical, confirmation_token, enabled, salt, last_login, password_requested_at, roles)');
+        $this->addSql('ALTER TABLE user DROP username_canonical, DROP email_canonical, DROP confirmation_token, DROP enabled, DROP salt, DROP last_login, DROP password_requested_at, DROP roles)');
     }
 }
