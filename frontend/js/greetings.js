748a2b2d9e93c3d516bd6053b1170649dcd72992
Tom <mail@alpers.it>
2018-02-22 20:29:21 +0100

    hello React!

---
+++ frontend/js/greetings.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class Greetings extends React.Component {
+    render() {
+        return <h1>Hellooho, greetings from React!</h1>
+    }
+}
---
+++ frontend/js/main.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Greetings from "./greetings";
+
+ReactDOM.render(
+    <Greetings />,
+    document.getElementById("react")
+);
