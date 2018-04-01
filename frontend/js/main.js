376541b83e0b4f26f9e85bc54dbba738dc2fbb81
Tom <mail@alpers.it>
2018-04-01 17:41:32 +0200

    fixed: removed react test element

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -3,11 +3,6 @@ import ReactDOM from "react-dom";
 import Greetings from "./greetings";
 import Administration from "./components/Administration";
 
-ReactDOM.render(
-    <Greetings />,
-    document.getElementById("react")
-);
-
 if ($('#admin-page').length) {
     ReactDOM.render(
         <Administration />,
