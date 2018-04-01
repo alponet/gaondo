bc965ea93037ceefd3d467b4aaccf2908aa756fa
Tom <mail@alpers.it>
2018-04-01 21:42:31 +0200

    remove obsolete react test component

--- frontend/js/greetings.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default class Greetings extends React.Component {
-    render() {
-        return <h1>Hellooho, greetings from React!</h1>
-    }
-}
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Greetings from "./greetings";
 import Administration from "./components/Administration";
 
 if ($('#admin-page').length) {
