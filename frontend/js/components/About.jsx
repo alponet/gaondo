a5bdc6874aa6a61fe74a166ea67a13de4340f75c
Brian Gomes Bascoy <pera@users.noreply.github.com>
2018-06-27 15:01:52 -0700

    Adding more description

--- frontend/js/components/About.jsx
+++ frontend/js/components/About.jsx
@@ -26,7 +26,7 @@ export default class About extends React.Component {
                     <h2>¡Bienvenid@!</h2>
                 </div>
                 <div className="content" onClick={() => this.toggleText()} style={{ cursor: "pointer" }}>
-                    <h3>¿Que es Gaondo?</h3>
+                    <h3>¿Que es Gaondo? Clickeá acá y te contamos...</h3>
                     <div style={{ display: this.state.showText ? "block" : "none" }}>
                         <p>Gaondo es una comunidad online dedicada a los memes y el humor argentino, hecho por y para nosotr@s. Particularmente nos interesa el humor político, social y futbolístico, pero reír es reír, así que otras temáticas también nos divierten.</p>
                         <p>Crea una cuenta para poder postear, comentar y votar. ¿Sabías que tenemos nuestro propio Creador de Memes? Una vez hecho el posteo, podés compartirlo directamente en tu cuenta de Twitter y Facebook clickeando en los iconos de abajo.</p>
